f283930e9161cf157b18395c5693c1b4cc1caddc
Eddie Sunden <eddie.bergstrandsunden@elev.ga.ntig.se>
2022-08-25 09:48:44 +0200

    Lektion 3

---
+++ Isak.py
@@ -0,0 +1,6 @@
+name = input("Hej vad heter du?")
+if name in ["Frozen", "frozen", "Frozen plays", "frozen plays", "Isak", "isak"]: 
+    print("Du är Isak")
+else: 
+    print("Välkommen!", name)
+age = input(f"Hur gammal är du {name}? ")
